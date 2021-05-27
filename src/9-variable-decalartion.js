380680ea76a50391d7e4c70e93c23e07c3ba5556
santosh <santosh.ece06@gmail.com>
2021-05-27 12:02:57 +0530

    basics of collection.

---
+++ src/9-variable-decalartion.js
@@ -0,0 +1,11 @@
+// basic data type declaration
+let title = "My Project";
+let counter = 100;
+let price = 99.99;
+
+// advance function as an object;
+let af = function() {};
+let lm = ()=>{};
+
+// collection or arryas in javascript.
+let list = [];
